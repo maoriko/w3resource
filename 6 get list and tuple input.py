df1086ff345e9d22543efaf1a851ee313b395d33
unknown <maor.paz@pc4us.co.il>
2019-09-27 22:11:33 +0300

    get list and tuple

--- 6 get list and tuple input.py
+++ 6 get list and tuple input.py
@@ -1,6 +1,7 @@
 # Write a Python program which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers.
 
-input_list = input("to get list please enter your numbers: ")
-
-print(list(input_list))
-print(tuple(input_list))
+input_list = input("to get list please enter your numbers seperated with comma: ")
+list = input_list.split(",")
+tuple = tuple(list)
+print('List :', list)
+print('Tuple:', tuple)
