4f46e8cc544af9955e143da0d19b04bb842da772
unknown <maor.paz@pc4us.co.il>
2019-09-27 20:51:32 +0300

    set python as default in VS Code

---
+++ input from user first name and last name.py
@@ -0,0 +1,8 @@
+# Write a Python program which accepts the user's first and last name and print them in reverse order with a space between them
+
+def firstlast():
+    first_name = (input(str("Please enter your first name:")))
+    last_name = (input(str("Now enter your last name:")))
+    print(last_name, first_name)
+
+firstlast()
